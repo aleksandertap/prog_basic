9be6ab0639a5db6c2a29f465556601b9d3fd9785
aleksander <aleksander@aleksander.aleksander>
2024-09-27 08:37:30 -0400

    javascripti asjad

---
+++ index.js
@@ -0,0 +1,87 @@
+//This is my first js code
+        
+console.log("Hello World");
+
+// let name = "Aleksander";
+// console.log(name);
+
+// let firstName = "Aleksander", lastName = "Täpsi";
+// ei ole soovituslik nii
+
+// let firstName = "Aleksander";
+// let lastName = "Täpsi";
+// console.log(firstName, lastName);
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+// annab errori
+
+// //primitive/value types
+// let name1 = "Aleks"; // string literal
+// // let age = 30;  // number literal
+// let isApproved = false; // Boolean literal
+// let firstName1 = undefined; //määramata väärtus
+// let selectedColor = null; //väärtus null - tuleb hiljem maybe
+
+//reference types
+//dynamic
+
+let person = {
+    name: "Aleksander",
+    age: "30"
+}
+
+// dot notation
+person.name = "John";
+
+//bracket notation
+let selection = "name";
+person[selection] = "Mary";
+
+console.log(person.name);
+
+
+// arrays
+
+let selectedColors = ["red", "blue"];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
+
+// let number1 = prompt("a+ ");
+// let number2 = prompt("b");
+// number1= Number (number1);
+// number2= Number(number2);
+// answer = number1 + number2;
+// document.write("summa on " + answer);
+
+
+let a = 10;
+let b = 20;
+document.write(a + "<br>" + b + "<br>")
+a = 20;
+b = 10;
+
+document.write("a on nüüd " + a + "<br>" + "b on nüüd " + b )
+
+
+// functions
+
+//performing task
+function greet(name, lastName){
+    console.log("Hello " + name + " "+ lastName);
+}
+greet("Aleksander", "Täpsi");
+greet("Maret");
+
+//calculating a value
+function square(number){
+    return number * number;
+}
+
+console.log(square(6));
