9307bcb652cedfe90c1cec21effe09b737698e09
aleksander <aleksander@aleksander.aleksander>
2024-10-20 08:50:13 -0400

    uuesti tehtud

--- tund4/nrggame.js
+++ tund4/nrggame.js
@@ -2,20 +2,32 @@ const min = 1;
 const max = 100;
 let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
 console.log(randomNum);
-let guess;
-do{
-  guess = parseInt(prompt("paku number"));
 
-  if (guess === randomNum) {
-    alert("Sisestasid õige arvu");
-  } 
-  else if (guess > randomNum){
-    alert("Pakkusid liiga suure numbri, paku uuesti ")
-    
+for (let i = 10; i >0; i--){
+  let guess = prompt("Paku üks number 1-100");
+  
+  
+  if(guess === null){
+    window.alert(`Andsid alla, vastus oli ${randomNum}`)
+    break;
   }
+  
+guess= parseInt(guess);
+
+  if(guess === randomNum){
+    window.alert("Pakkusid õige numbri");
+    break;
+  }
+  
   else if (guess < randomNum){
-    alert("Sisestasid väiksema numbri, paku uuesti")
-    
+    window.alert(`Pakkusid liiga väikse arvu, sul on jäänud ${i - 1} arvamist`);
+  }
+  else if(guess > randomNum){
+    window.alert(`Pakkusid liiga suure numbri, sul on jäänud ${i - 1} arvamist`);
+  }
+  
+  else{
+    window.alert(`Sa ei sisestanud numbrit. Sul on jäänud ${i - 1} korda.`);
+
   }
 }
-while(guess !== randomNum)
